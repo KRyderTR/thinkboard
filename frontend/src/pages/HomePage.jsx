d063fa5c0308988299ff3e40d4774b097b3ee281
KRyderTR <charitrom@gmail.com>
2025-06-17 16:26:27 +0300

    thinkboard-complete

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from "../components/RateLimitedUI";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -44,11 +45,13 @@ const HomePage = () => {
           <div className="text-center text-primary p-10">Loading notes...</div>
         )}
 
+        {notes.length === 0  && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
               <div>
-                <NoteCard key={note._id} note={note} />
+                <NoteCard key={note._id} note={note} setNotes={setNotes} />
               </div>
             ))}
           </div>
